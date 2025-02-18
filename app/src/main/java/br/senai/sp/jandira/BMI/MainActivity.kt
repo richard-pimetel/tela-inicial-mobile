a83470ff26d0f15264647e1277caddbfdae394f7
richard-pimetel <richardpiment230@gmail.com>
2025-02-18 17:17:01 -0300

    tela home concluida

---
+++ app/src/main/java/br/senai/sp/jandira/BMI/MainActivity.kt
@@ -0,0 +1,43 @@
+package br.senai.sp.jandira.BMI
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.BMI.screens.HomeScreens
+import br.senai.sp.jandira.BMI.ui.theme.BMITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BMITheme {
+                HomeScreens()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BMITheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/br/senai/sp/jandira/BMI/screens/HomeScreens.kt
@@ -0,0 +1,140 @@
+package br.senai.sp.jandira.BMI.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.BMI.R
+
+@Composable
+fun HomeScreens(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.horizontalGradient(
+                listOf(
+                    Color(0xFF324B33),
+                    Color(0xFF1CA121)
+                )
+            ))
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+
+
+        ) {
+            Image(
+
+                painter = painterResource(R.drawable.atleta),
+                contentDescription = stringResource(R.string.logo),
+                modifier = Modifier
+                    .padding(vertical = 60.dp)
+
+            )
+            Text(
+                text = stringResource(R.string.welcome),
+                fontSize = 48.sp,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(bottom = 60.dp)
+            )
+
+            Card(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(100.dp),
+                    shape = RoundedCornerShape(
+                        topStart = 48.dp,
+                        topEnd = 48.dp
+                    )
+
+            ) {
+                Column {
+                    Column(
+                        modifier = Modifier
+                            .padding(start = 40.dp,
+                                top = 40.dp
+                            )
+                    ) {
+                        Text(
+                        text = stringResource(R.string.your_name),
+                        fontSize = 30.sp,
+                        modifier = Modifier
+
+
+                        )
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = { },
+                            label = {
+                                Text(text = "Digite o seu nome ")
+                            },
+
+
+
+
+                        )
+
+
+                        }
+
+                    Column (
+                        modifier = Modifier
+                            .padding(top = 220.dp,
+                                start = 300.dp)
+                    ){
+                        Button(
+                            onClick = {},
+                            shape = RoundedCornerShape(15.dp)
+                        ) {
+                        Text(text = stringResource(R.string.next))
+                            Icon(
+                                imageVector = Icons.Filled.ArrowForward,
+                                contentDescription = ""
+
+                            )
+
+                    } }
+
+
+
+
+
+                }
+
+            }
+        }
+    }
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreensPreview() {
+    HomeScreens()
+}
